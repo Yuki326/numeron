be6fd5fafadd83e9b1c2b4a26a090ee3c10cf9b6
Yuki326 <s20a1035@gmail.com>
2022-02-05 02:01:17 +0900

    css適用、コンポーネント呼び出し

--- pages/History.js
+++ pages/History.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./History.css";
+
 
 
 class History extends Component {
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,17 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 import React, { Component } from "react";
-// import "./App.css";
+
 import Numeron from "./Numeron.js";
-// import Timer from "./Timer.js";
-// import History from "./History.js";
+import Timer from "./Timer.js";
+import History from "./History.js";
 class App extends Component {
   render() {
     return (
       <div className="body">
         <Numeron />
-        
+        <History />
+        <Timer />
         <Link href="/other/other">
         <a>ほかのページへ</a>
         </Link>
